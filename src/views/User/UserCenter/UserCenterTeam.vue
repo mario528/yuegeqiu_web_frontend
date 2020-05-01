1caa281d41257e6fd64e93fd1031b60622c2d6ff
马加奥 <majiaao@spacebox.fun>
2020-05-02 02:05:06 +0800

    update: 上传部分代码

--- src/views/User/UserCenter/UserCenterTeam.vue
+++ src/views/User/UserCenter/UserCenterTeam.vue
@@ -2,14 +2,18 @@
  * @Author: majiaao
  * @Date: 2020-04-28 21:06:30
  * @LastEditors: majiaao
- * @LastEditTime: 2020-05-01 23:17:10
+ * @LastEditTime: 2020-05-02 02:04:10
  * @Description: file content
  -->
 <template>
     <div class="flex-column-center container">
         <div class="width-100 flex-x-start user-center-title">我的球队</div>
         <div v-if="teamList.length == 0" class="empty-area">
-            <img class="empty-area-icon" :src="require('@/assets/empty_box.png')">
+            <div class="flex-row-y-center empty-area-tips-line">
+                <div class="empty-area-tips-btn" @click="handleRouter('create_team')">创建球队</div>
+                <img class="empty-area-icon" :src="require('@/assets/empty_box.png')">
+                <div class="empty-area-tips-btn" @click="handleRouter('join_team')">加入球队</div>
+            </div>
             <div class="empty-area-text">您暂时未加入任何球队</div>
         </div>
     </div>
@@ -35,35 +39,78 @@ export default class UserCenterTeam extends Vue {
       this.teamList = res.team;
     });
   }
+  handleRouter(type: string) {
+    switch (type) {
+      case "create_team":
+        this.$router.push('/team')
+        break;
+      case "join_team":
+        break;
+    }
+  }
 }
 </script>
 <style lang="scss" scoped>
 .container {
-    padding: 20px;
+  padding: 20px;
 }
 .empty-area {
-  color: #a9a9a9;
+  color: $high_light_color;
   text-align: center;
+  font-weight: 500;
   &-icon {
-    margin: 10vh auto 2vh auto;
     width: 5vw;
     height: 5vw;
-    min-width: 40px;
-    min-height: 40px;
+    min-width: 55px;
+    min-height: 55px;
+    padding: 0 5px;
+  }
+  &-tips {
+    &-btn {
+      display: inline-block;
+      position: relative;
+      margin: 0 10px;
+      font-size: 13px;
+      background-color: $base_color;
+      color: white;
+      padding: 5px 10px;
+      border-radius: 5px;
+      width: 70px;
+      animation: crosswiseSlider 1.5s ease-in-out;
+      overflow: hidden;
+      display: block;
+      white-space: nowrap;
+    }
   }
 }
 @media screen and (max-width: 400px) {
   .empty-area {
-    &-icon {
-      margin: 10px auto 10px auto;
+    padding: 30px 0;
+    &-tips {
+      &-line {
+        padding: 30px 0;
+      }
     }
   }
 }
 @media screen and (min-width: 401px) {
   .empty-area {
-    &-icon {
-      margin: 10vh auto 2vh auto;
+    padding: 15vh 0;
+    &-tips {
+      &-line {
+        margin-bottom: 5vh;
+      }
     }
+  }
+}
+@keyframes crosswiseSlider {
+  0% {
+    width: 0;
+    height: 100%;
+  }
+  100% {
+    width: 70px;
+    height: 100%;
   }
 }
 </style>
