b9b90c60df0ef72fb57b5e7ef16e780e82d60b67
马加奥 <majiaao@spacebox.fun>
2020-06-17 18:07:01 +0800

    chore: 调通wss

--- src/main.ts
+++ src/main.ts
@@ -48,7 +48,8 @@ import Loading from './components/Loading/libs/loading'
 import Dialog from './components/Dialog/libs/dialog'
 const isProductionEnv = process.env.NODE_ENV == 'production'
 // add websocket
-const io = isProductionEnv ? socketio('https://www.yuegeqiu.club:3000') : socketio('https://localhost:3000')
+// const io = isProductionEnv ? socketio('www.yuegeqiu.club/yq') : socketio('http://localhost:3000')
+const io = isProductionEnv ? socketio('www.yuegeqiu.club') : socketio('http://localhost:3000')
 // const io = socketio('http://101.201.142.182:3000')
 
 Vue.config.productionTip = false
